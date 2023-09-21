85f17c74dc2cae8a958ea9c5cf9462e6fa9feb77
0000masa <masanori.basketball@gmail.com>
2023-09-21 17:19:48 +0900

    google認証

--- app/Http/Controllers/GoogleLoginController.php
+++ app/Http/Controllers/GoogleLoginController.php
@@ -5,31 +5,52 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
+//use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Laravel\Socialite\Facades\Socialite;
-
+//use Socialite; 
 
 class GoogleLoginController extends Controller
 {
-    /*public function getGoogleAuth()
+    
+    //use AuthenticatesUsers;
+    
+    
+    //protected $redirectTo = RouteServiceProvider::HOME;
+      
+    
+    public function __construct()
     {
-        return Socialite::driver('google')
-            ->redirect();
+        $this->middleware('guest')->except('logout');
     }
     
-    public function authGoogleCallback()
+   public function redirectToGoogle()
     {
-        $googleUser = Socialite::driver('google')->stateless()->user();
-        $user = User::firstOrCreate([
-            'email' => $googleUser->email
-        ], [
-            'email_verified_at' => now(),
-            'name' => $googleUser->name,
-            'google_id' => $googleUser->getId()
+        return Socialite::driver('google')->redirect();
+    }
+    
+    public function handleGoogleCallback()
+    {
+        $gUser = Socialite::driver('google')->stateless()->user();
+        // email が合致するユーザーを取得
+        $user = User::where('email', $gUser->email)->first();
+        // 見つからなければ新しくユーザーを作成
+        if ($user == null) {
+            $user = $this->createUserByGoogle($gUser);
+        }
+        // ログイン処理
+        \Auth::login($user, true);
+        return redirect('/');
+    }
+    
+    public function createUserByGoogle($gUser)
+    {
+        return User::create([
+            'name' => $gUser->name,
+            'email' => $gUser->email,
+            'email_verified_at' => now(),//追加
+            'password' => \Hash::make(uniqid()),
         ]);
-        Auth::login($user, true);
-        return view('posts.index');
-       
-    }*/
+    }
     
     
     
--- routes/web.php
+++ routes/web.php
@@ -75,8 +75,8 @@ Route::middleware('verified')->group(function(){
     Route::put('/userprofile/{profile}', [UserprofileController::class, 'update']);
 });
 
-//Route::get('/auth/redirect', [GoogleLoginController::class, 'getGoogleAuth']);
-//Route::get('/login/callback', [GoogleLoginController::class, 'authGoogleCallback']);
+Route::get('/login/google', [GoogleLoginController::class, 'redirectToGoogle']);
+Route::get('/login/google/callback', [GoogleLoginController::class, 'handleGoogleCallback']);
 
 //Route::get('/posts/{article}', [ArticleController::class ,'show']);
  
